3d88734ba49dd89279bc38bb8e7a769b409fda32
Ivan <covraworks@gmail.com>
2024-05-23 19:23:56 +0200

    main

---
+++ code0.js
@@ -0,0 +1,123 @@
+gdjs.SplashSceneCode = {};
+gdjs.SplashSceneCode.GDbackgroundDottedObjects1= [];
+gdjs.SplashSceneCode.GDbackgroundDottedObjects2= [];
+gdjs.SplashSceneCode.GDbackgroundDottedObjects3= [];
+gdjs.SplashSceneCode.GDTabalotIconObjects1= [];
+gdjs.SplashSceneCode.GDTabalotIconObjects2= [];
+gdjs.SplashSceneCode.GDTabalotIconObjects3= [];
+gdjs.SplashSceneCode.GDtxt_9595debugObjects1= [];
+gdjs.SplashSceneCode.GDtxt_9595debugObjects2= [];
+gdjs.SplashSceneCode.GDtxt_9595debugObjects3= [];
+
+
+gdjs.SplashSceneCode.userFunc0x81d6d0 = function GDJSInlineCode(runtimeScene) {
+"use strict";
+var iframe = document.createElement("iframe");
+iframe.setAttribute("width", "1080");
+iframe.setAttribute("height", "720");
+iframe.align = "center";
+iframe.src = "https://hyplay.com/oauth/authorize/?appId=ae6b8795-169f-40c9-bf60-a3736dceeffc&chain=HYCHAIN_TESTNET&responseType=token&redirectUri=https://gd.games/covra/hyplay-test";
+
+document.body.appendChild(iframe);
+
+iframe.style.position = "absolute";
+iframe.style.zIndex = 1000;
+
+};
+gdjs.SplashSceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+}
+
+}
+
+
+{
+
+
+gdjs.SplashSceneCode.userFunc0x81d6d0(runtimeScene);
+
+}
+
+
+{
+
+
+
+}
+
+
+};gdjs.SplashSceneCode.eventsList1 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+{gdjs.evtTools.camera.hideLayer(runtimeScene, "IconLayer");
+}
+{ //Subevents
+gdjs.SplashSceneCode.eventsList0(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};gdjs.SplashSceneCode.eventsList2 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+{isConditionTrue_0 = runtimeScene.getOnceTriggers().triggerOnce(8977220);
+}
+}
+if (isConditionTrue_0) {
+
+{ //Subevents
+gdjs.SplashSceneCode.eventsList1(runtimeScene);} //End of subevents
+}
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+}
+
+}
+
+
+};
+
+gdjs.SplashSceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.SplashSceneCode.GDbackgroundDottedObjects1.length = 0;
+gdjs.SplashSceneCode.GDbackgroundDottedObjects2.length = 0;
+gdjs.SplashSceneCode.GDbackgroundDottedObjects3.length = 0;
+gdjs.SplashSceneCode.GDTabalotIconObjects1.length = 0;
+gdjs.SplashSceneCode.GDTabalotIconObjects2.length = 0;
+gdjs.SplashSceneCode.GDTabalotIconObjects3.length = 0;
+gdjs.SplashSceneCode.GDtxt_9595debugObjects1.length = 0;
+gdjs.SplashSceneCode.GDtxt_9595debugObjects2.length = 0;
+gdjs.SplashSceneCode.GDtxt_9595debugObjects3.length = 0;
+
+gdjs.SplashSceneCode.eventsList2(runtimeScene);
+
+return;
+
+}
+
+gdjs['SplashSceneCode'] = gdjs.SplashSceneCode;
---
+++ code1.js
@@ -0,0 +1,110 @@
+gdjs.MainCode = {};
+gdjs.MainCode.GDtxt_9595tituloObjects1= [];
+gdjs.MainCode.GDtxt_9595tituloObjects2= [];
+gdjs.MainCode.GDtxt_9595tituloObjects3= [];
+
+
+gdjs.MainCode.eventsList0 = function(runtimeScene, asyncObjectsList) {
+
+{
+
+
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+}
+
+}
+
+
+};gdjs.MainCode.asyncCallback8996980 = function (runtimeScene, asyncObjectsList) {
+
+{ //Subevents
+gdjs.MainCode.eventsList0(runtimeScene, asyncObjectsList);} //End of subevents
+}
+gdjs.MainCode.eventsList1 = function(runtimeScene) {
+
+{
+
+
+{
+{
+const asyncObjectsList = new gdjs.LongLivedObjectsList();
+runtimeScene.getAsyncTasksManager().addTask(gdjs.evtsExt__AdvancedHTTP__ExecuteRequest.func(runtimeScene, "HYPLAYLogin", runtimeScene.getScene().getVariables().get("statusResponse"), (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)), (runtimeScene) => (gdjs.MainCode.asyncCallback8996980(runtimeScene, asyncObjectsList)));
+}
+}
+
+}
+
+
+};gdjs.MainCode.eventsList2 = function(runtimeScene) {
+
+{
+
+
+
+}
+
+
+{
+
+
+
+}
+
+
+{
+
+
+
+}
+
+
+{
+
+
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+{gdjs.evtsExt__AdvancedHTTP__CreateRequest.func(runtimeScene, "HYPLAYLogin", gdjs.evtTools.variable.getVariableString(runtimeScene.getScene().getVariables().getFromIndex(3)), (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined));
+}{gdjs.evtsExt__AdvancedHTTP__SetRequestMethod.func(runtimeScene, "HYPLAYLogin", "GET", (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined));
+}{gdjs.evtsExt__AdvancedHTTP__SetRequestHeader.func(runtimeScene, "application/json", "HYPLAYLogin", "Content-Type", (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined));
+}{gdjs.evtsExt__AdvancedHTTP__SetRequestHeader.func(runtimeScene, "user_at_kKbC7274_LV1SU4bHupVLzZglUEgUcluxE9ocauom_eA0PY7r-hoPszGqke5cNRz", "HYPLAYLogin", "Authorization", (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined));
+}
+{ //Subevents
+gdjs.MainCode.eventsList1(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};
+
+gdjs.MainCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.MainCode.GDtxt_9595tituloObjects1.length = 0;
+gdjs.MainCode.GDtxt_9595tituloObjects2.length = 0;
+gdjs.MainCode.GDtxt_9595tituloObjects3.length = 0;
+
+gdjs.MainCode.eventsList2(runtimeScene);
+
+return;
+
+}
+
+gdjs['MainCode'] = gdjs.MainCode;
