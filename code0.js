8d05aae10898fb9bf97978739ed01d5b062fe446
Ivan <covraworks@gmail.com>
2024-05-24 09:38:41 +0200

    last

--- code0.js
+++ code0.js
@@ -8,9 +8,12 @@ gdjs.SplashSceneCode.GDTabalotIconObjects3= [];
 gdjs.SplashSceneCode.GDtxt_9595debugObjects1= [];
 gdjs.SplashSceneCode.GDtxt_9595debugObjects2= [];
 gdjs.SplashSceneCode.GDtxt_9595debugObjects3= [];
+gdjs.SplashSceneCode.GDloginObjects1= [];
+gdjs.SplashSceneCode.GDloginObjects2= [];
+gdjs.SplashSceneCode.GDloginObjects3= [];
 
 
-gdjs.SplashSceneCode.userFunc0x815118 = function GDJSInlineCode(runtimeScene, objects) {
+gdjs.SplashSceneCode.userFunc0x93f958 = function GDJSInlineCode(runtimeScene, objects) {
 "use strict";
     var appId = "ae6b8795-169f-40c9-bf60-a3736dceeffc";
     var redirectUri = encodeURIComponent("https://covra.github.io/testHYPLAY/redirect.html"); // Add an empty html file at this URL, and add it to your hyplay app
@@ -36,14 +39,15 @@ gdjs.SplashSceneCode.userFunc0x815118 = function GDJSInlineCode(runtimeScene, ob
                 var params = new URLSearchParams(hash);
                 var accessToken = params.get('token');
 
-                if (accessToken) {
-                    console.log('Access token:', accessToken);
+                if (accessToken) {                    
                     // However gdevelop wants you to send a message bacn to your game
                     popup.close();
                     clearInterval(interval);
                     // Checking 
-                    objects[0].setString("Token succesful: " + accessToken);
+                    objects[0].setString("Successful authentication! ", accessToken);
                     runtimeScene.setBackgroundColor(0,204,100);
+                    runtimeScene.getGame().getVariables().get("sessionAccessToken").setString(accessToken);
+                    console.log('Access token:', accessToken);
                 }
             }
         } catch (e) {
@@ -61,67 +65,11 @@ gdjs.SplashSceneCode.eventsList0 = function(runtimeScene) {
 
 {
 
-gdjs.copyArray(gdjs.SplashSceneCode.GDtxt_9595debugObjects1, gdjs.SplashSceneCode.GDtxt_9595debugObjects2);
-
+/* Reuse gdjs.SplashSceneCode.GDtxt_9595debugObjects2 */
 
 var objects = [];
 objects.push.apply(objects,gdjs.SplashSceneCode.GDtxt_9595debugObjects2);
-gdjs.SplashSceneCode.userFunc0x815118(runtimeScene, objects);
-
-}
-
-
-{
-
-
-let isConditionTrue_0 = false;
-{
-}
-
-}
-
-
-{
-
-
-
-}
-
-
-{
-
-
-
-}
-
-
-{
-
-
-
-}
-
-
-{
-
-
-
-}
-
-
-{
-
-
-let isConditionTrue_0 = false;
-{
-}
-
-}
-
-
-{
-
-
+gdjs.SplashSceneCode.userFunc0x93f958(runtimeScene, objects);
 
 }
 
@@ -133,10 +81,25 @@ let isConditionTrue_0 = false;
 
 let isConditionTrue_0 = false;
 {
-gdjs.copyArray(runtimeScene.getObjects("txt_debug"), gdjs.SplashSceneCode.GDtxt_9595debugObjects1);
+gdjs.copyArray(gdjs.SplashSceneCode.GDloginObjects1, gdjs.SplashSceneCode.GDloginObjects2);
+
+{for(var i = 0, len = gdjs.SplashSceneCode.GDloginObjects2.length ;i < len;++i) {
+    gdjs.SplashSceneCode.GDloginObjects2[i].Activate(false, (typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined));
+}
+}}
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+gdjs.copyArray(runtimeScene.getObjects("txt_debug"), gdjs.SplashSceneCode.GDtxt_9595debugObjects2);
 {gdjs.evtTools.camera.hideLayer(runtimeScene, "IconLayer");
-}{for(var i = 0, len = gdjs.SplashSceneCode.GDtxt_9595debugObjects1.length ;i < len;++i) {
-    gdjs.SplashSceneCode.GDtxt_9595debugObjects1[i].setColor("255;255;255");
+}{for(var i = 0, len = gdjs.SplashSceneCode.GDtxt_9595debugObjects2.length ;i < len;++i) {
+    gdjs.SplashSceneCode.GDtxt_9595debugObjects2[i].setColor("255;255;255");
 }
 }
 { //Subevents
@@ -146,17 +109,35 @@ gdjs.SplashSceneCode.eventsList0(runtimeScene);} //End of subevents
 }
 
 
-};gdjs.SplashSceneCode.eventsList2 = function(runtimeScene) {
-
 {
 
 
 let isConditionTrue_0 = false;
+{
+}
+
+}
+
+
+};gdjs.SplashSceneCode.eventsList2 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("login"), gdjs.SplashSceneCode.GDloginObjects1);
+
+let isConditionTrue_0 = false;
 isConditionTrue_0 = false;
-isConditionTrue_0 = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+for (var i = 0, k = 0, l = gdjs.SplashSceneCode.GDloginObjects1.length;i<l;++i) {
+    if ( gdjs.SplashSceneCode.GDloginObjects1[i].IsPressed((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)) ) {
+        isConditionTrue_0 = true;
+        gdjs.SplashSceneCode.GDloginObjects1[k] = gdjs.SplashSceneCode.GDloginObjects1[i];
+        ++k;
+    }
+}
+gdjs.SplashSceneCode.GDloginObjects1.length = k;
 if (isConditionTrue_0) {
 isConditionTrue_0 = false;
-{isConditionTrue_0 = runtimeScene.getOnceTriggers().triggerOnce(8989092);
+{isConditionTrue_0 = runtimeScene.getOnceTriggers().triggerOnce(9448380);
 }
 }
 if (isConditionTrue_0) {
@@ -165,6 +146,15 @@ if (isConditionTrue_0) {
 gdjs.SplashSceneCode.eventsList1(runtimeScene);} //End of subevents
 }
 
+}
+
+
+};gdjs.SplashSceneCode.eventsList3 = function(runtimeScene) {
+
+{
+
+
+gdjs.SplashSceneCode.eventsList2(runtimeScene);
 }
 
 
@@ -182,8 +172,11 @@ gdjs.SplashSceneCode.GDTabalotIconObjects3.length = 0;
 gdjs.SplashSceneCode.GDtxt_9595debugObjects1.length = 0;
 gdjs.SplashSceneCode.GDtxt_9595debugObjects2.length = 0;
 gdjs.SplashSceneCode.GDtxt_9595debugObjects3.length = 0;
+gdjs.SplashSceneCode.GDloginObjects1.length = 0;
+gdjs.SplashSceneCode.GDloginObjects2.length = 0;
+gdjs.SplashSceneCode.GDloginObjects3.length = 0;
 
-gdjs.SplashSceneCode.eventsList2(runtimeScene);
+gdjs.SplashSceneCode.eventsList3(runtimeScene);
 
 return;
 
